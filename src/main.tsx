2d75fe0f1fe478115e6bb92a3c6ffddb872038db
eduardo silveira <duzitz@gmail.com>
2025-05-06 02:25:13 -0300

    v

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
@@ -10,8 +10,6 @@ if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
